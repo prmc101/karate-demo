333f700aed1c5b584d293ecf0d81f423a12e428f
prmc101 <primecommunity101@gmail.com>
2023-06-10 16:32:06 +0100

    Git Demo feature

---
+++ src/main/java/com/prime/GitDemo.java
@@ -0,0 +1,11 @@
+package com.prime;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Karate demo feature");
+    }
+}
